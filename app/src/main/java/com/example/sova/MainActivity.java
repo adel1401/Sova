22e1f0eca57923c7074accd1219bc76c3eb38409
Adele2007 <adelyaalmazova@icloud.com>
2022-12-05 00:17:22 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sova/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView register;
+    private EditText Username, Email, Password;
+    Button Next;
+    }
